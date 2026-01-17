7362b0ff23dee73cca74de1d20752be52add0d76
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-01-17 23:36:43 +0800

    editing page

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -16,23 +16,34 @@
 	let { children } = $props();
 </script>
 
-<ModeWatcher />
-<header class="flex w-full justify-between items-center p-2 pt-4 px-6 mb-4">
-	<div class="flex gap-4 items-center text-sm">
-		<a href="/">
-			<Film />
-		</a>
-		<a href="/">Try</a>
-		<a href="/examples">Examples</a>
-		<a href="/docs">Docs</a>
-	</div>
-	<div class="flex gap-2 h-4 items-center">
-		<Button target="_blank" href="https://github.com/henrlly/DO-NOT-SUBMIT-2026" variant="ghost">
-			<GithubIcon class="size-5" />
-		</Button>
-		<Separator class="h-1/2" orientation="vertical" />
-		<ModeSwitcher />
-	</div>
-</header>
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<div class="flex h-screen w-full flex-col overflow-hidden">
+	<ModeWatcher />
+	<header class="flex w-full justify-between items-center p-2 pt-4 px-6 mb-4">
+		<div class="flex gap-4 items-center text-sm">
+			<a href="/">
+				<Film />
+			</a>
+			<a href="/">Try</a>
+			<a href="/examples">Examples</a>
+			<a href="/docs">Docs</a>
+		</div>
+		<div class="flex gap-2 h-4 items-center">
+			<Button target="_blank" href="https://github.com/henrlly/DO-NOT-SUBMIT-2026" variant="ghost">
+				<GithubIcon class="size-5" />
+			</Button>
+			<Separator class="h-1/2" orientation="vertical" />
+			<ModeSwitcher />
+		</div>
+	</header>
+	
+    <main class="flex-grow overflow-hidden">
+		{@render children()}
+    </main>
+</div>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<style>
+		/* Kill default browser margins that cause small scrolls */
+		body { margin: 0; padding: 0; overflow: hidden; }
+	</style>
+</svelte:head>
