652b39bc60e42fb90cf5630729a8fbc5d47ae78c
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-01-18 01:15:25 +0800

    found the bug, unity

--- frontend/src/lib/components/script-writing-display.svelte
+++ frontend/src/lib/components/script-writing-display.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { videoState } from "$lib/stores/generation-data.svelte";
-	import UnityPlayer from "$lib/components/UnityPlayer.svelte";
+	// import UnityPlayer from "$lib/components/UnityPlayer.svelte";
 
-    let unityPlayer: UnityPlayer | undefined = $state();
+    // let unityPlayer: UnityPlayer | undefined = $state();
 	let displayedScript = $state("");
 
 	function startTypewriter(text: string, speed = 1) {
@@ -25,16 +25,16 @@
         }
     });
 	
-	$effect(() => {
-		setTimeout(() => {
-			unityPlayer?.changeBackgroundColor("#09090b");
-			unityPlayer?.startTyping(videoState.full_script);
-		}, 5000)
-	})
+	// $effect(() => {
+	// 	setTimeout(() => {
+	// 		unityPlayer?.changeBackgroundColor("#09090b");
+	// 		unityPlayer?.startTyping(videoState.full_script);
+	// 	}, 5000)
+	// })
 </script>
 
 <div class="flex flex-col justify-center items-center w-full h-[90vh] gap-6">
 	<div class="h-full w-[90%] rounded-3xl">
-		<UnityPlayer bind:this={unityPlayer}/>
+		<!-- <UnityPlayer bind:this={unityPlayer}/> -->
 	</div>
 </div>
