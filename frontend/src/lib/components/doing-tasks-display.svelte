1fe4f4c854146fd93b534c50c98318c579acceca
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-01-17 22:19:18 +0800

    doing tasks page

--- frontend/src/lib/components/doing-tasks-display.svelte
+++ frontend/src/lib/components/doing-tasks-display.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
+	import { videoState } from "$lib/stores/generation-data.svelte";
 
 </script>
 
 <p>hi from doing tasks</p>
+<div class="w-full grid grid-cols-3 gap-4 p-4">
+	<div class="flex flex-col justify-between">
+		<p>{JSON.stringify(videoState.narration_gen)}</p>
+		<p>Recording Voiceover...</p>
+	</div>
+	<div class="flex flex-col justify-between">
+		<p class="overflow-auto">{JSON.stringify(videoState.visual_asset_gen)}</p>
+		<p>Picking GIFs...</p>
+	</div>
+	<div class="flex flex-col justify-between">
+		<p>{JSON.stringify(videoState.sfx_asset_gen)}</p>
+		<p>Recording SFXs...</p>
+	</div>
+</div>
