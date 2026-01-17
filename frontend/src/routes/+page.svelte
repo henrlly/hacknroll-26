95739ed4c34572dde6fa719841342eece3aa60de
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-01-18 02:18:25 +0800

    overflow auto

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,13 +5,39 @@
 	import DoingTasksDisplay from '$lib/components/doing-tasks-display.svelte';
 	import FinalVideo from '$lib/components/completed-stage.svelte';
 	import { videoState } from '$lib/stores/generation-data.svelte';
+	import UnityPlayer from "$lib/components/UnityPlayer.svelte";
+	
+	let unityPlayer: UnityPlayer | undefined = $state();
+	let unityPlayerHidden = $state(true);
+	let character = $state("trump" as "trump" | "obama");
+	$effect(() => {
+		setTimeout(() => {
+			unityPlayer?.changeBackgroundColor("#000000");
+		}, 5000)
+	})
+
+	$effect(() => {
+		if (videoState.generationStepView === "COMPLETED") {
+			unityPlayerHidden = true;
+		} else if (videoState.generationStepView === "WRITING SCRIPT") {
+			unityPlayerHidden = false;
+			unityPlayer?.changeCharacter(character);
+			unityPlayer?.startTyping(videoState.full_script);
+		} else if (videoState.generationStepView === "DOING TASKS") {
+			unityPlayer?.startNarration(videoState.full_script);
+		} else {
+			
+		}
+	})
 </script>
 
 <div class="flex h-screen w-full flex-col items-center gap-4">
 	<ProgressFlow bind:generationStep={videoState.generationStepView} />
-
-	{#if videoState.generationStepView === 'INPUT'}
-		<PromptInput />
+	<div class="h-[80%] w-[90%] rounded-3xl">
+		<UnityPlayer bind:this={unityPlayer}/>
+	</div>
+		{#if videoState.generationStepView === 'INPUT'}
+		<PromptInput bind:character={character} />
 		{:else if videoState.generationStepView === 'WRITING SCRIPT'}
 		<ScriptWritingData />
 		{:else if videoState.generationStepView === 'DOING TASKS'}
