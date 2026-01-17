5aaef2228dc6045d4f9f66ec5cc236233d487c0e
Henry <80515759+henrlly@users.noreply.github.com>
2026-01-17 21:30:45 +0800

    add api types for editing

---
+++ frontend/src/lib/utils/editing.ts
@@ -0,0 +1,23 @@
+import { socketState } from '$lib/socket.svelte';
+import { EditSceneRequest, SelectImageRequest } from '$lib/types/apiTypes';
+
+export function sendEditPrompt(prompt: string) {
+	const request = EditSceneRequest.parse({
+		custom_edit_prompt: prompt
+	});
+	socketState.socket?.send(JSON.stringify(request));
+}
+
+export function selectVisual({
+	assetId,
+	selectedCandidateId
+}: {
+	assetId: string;
+	selectedCandidateId: string;
+}) {
+	const request = SelectImageRequest.parse({
+		asset_id: assetId,
+		selected_candidate_id: selectedCandidateId
+	});
+	socketState.socket?.send(JSON.stringify(request));
+}
