9870f81f4d305c77bf11c52e34b018bc993ddb37
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-01-17 18:19:02 +0800

    some frontend refactoring

---
+++ frontend/src/lib/components/prompt-input.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card/index.js";
+    import { Textarea } from "$lib/components/ui/textarea/index.js";
+	import { buttonVariants } from "$lib/components/ui/button";
+    import { Badge } from "$lib/components/ui/badge/index.js";
+    import { socketState } from "$lib/socket.svelte";
+
+    let voice = $state("trump");
+    let prompt = $state("");
+
+    async function handleSubmit() {
+        if (socketState.status === "connected" && socketState.socket) {
+            const payload = { prompt, voice };
+            socketState.socket.send(JSON.stringify(payload));
+        } else {
+            console.error("WebSocket is not connected.");
+        }
+    }
+</script>
+    
+<Card.Root class="w-2/3">
+    <Card.Header>
+        <Card.Title>Enter Prompt</Card.Title>
+        <Card.Description>Describe what the video should explain</Card.Description>
+    </Card.Header>
+    <Card.Content>
+        <form class="flex flex-col items-start" onsubmit={handleSubmit}>
+        <Textarea class="mb-4 hover:shadow-primary hover:shadow-md focus:shadow-primary focus:shadow-md" name="prompt" placeholder="Enter video idea" bind:value={prompt}/>
+
+            <input type="hidden" name="voice" bind:value={voice}>
+
+            <div class="w-full flex justify-between">
+                <div class="flex gap-2">
+                <Badge class="h-6" variant={voice === "trump" ? "default" : "outline"} onclick={() => { voice = "trump" }}>Trump</Badge>
+                <Badge class="h-6" variant={voice === "peter" ? "default" : "outline"} onclick={() => { voice = "peter" }}>Peter</Badge>
+                <Badge class="h-6" variant={voice === "obama" ? "default" : "outline"} onclick={() => { voice = "obama" }}>Obama</Badge>
+                </div>
+                <button class="w-20 {buttonVariants({ variant: "default" })}">Submit</button>
+            </div>
+        </form>
+    </Card.Content>
+</Card.Root>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,71 +1,13 @@
 <script lang="ts">
-    import * as Card from "$lib/components/ui/card/index.js";
-    import { Textarea } from "$lib/components/ui/textarea/index.js";
-	import { buttonVariants } from "$lib/components/ui/button";
-    import { Badge } from "$lib/components/ui/badge/index.js";
-	import { onMount } from "svelte";
+    import PromptInput from "$lib/components/prompt-input.svelte";
     import { socketState } from "$lib/socket.svelte";
 
-    let submitted = $state(false);
-
     let sessionID: string = $state("");
-    let generatedScript: string = $state("");
-    let displayedScript: string = $state("");
-
-    onMount(() => {
-
-    })
-
-    let voice = $state("trump");
-    let prompt = $state("");
-
-    function selectVoice(selectedVoice: string) {
-        return () => { voice = selectedVoice; };
-    }
-
-    async function handleSubmit() {
-        if (socketState.status === "connected" && socketState.socket) {
-            const payload = { prompt, voice };
-            socketState.socket.send(JSON.stringify(payload));
-            submitted = true;
-        } else {
-            console.error("WebSocket is not connected.");
-        }
-    }
+    let generationStep = $state("idle" as "idle" | "writing_script" | "doing_tasks" | "complete");
 </script>
 
 <div class="w-full h-screen flex flex-col gap-4 items-center">
-    <Card.Root class="w-2/3">
-        {#if submitted}
-        <Card.Header>
-          <Card.Title>Generated Script</Card.Title>
-          <Card.Description>We're cooking...</Card.Description>
-        </Card.Header>
-        {:else}
-        <Card.Header>
-          <Card.Title>Enter Prompt</Card.Title>
-          <Card.Description>Describe what the video should explain</Card.Description>
-        </Card.Header>
-        {/if}
-      <Card.Content>
-          <form class="flex flex-col items-start" onsubmit={handleSubmit}>
-            {#if submitted}
-            <Textarea class="mb-4" name="prompt" placeholder="Enter video idea" bind:value={displayedScript}/>
-            {:else}
-            <Textarea class="mb-4 hover:shadow-primary hover:shadow-md focus:shadow-primary focus:shadow-md" name="prompt" placeholder="Enter video idea" bind:value={prompt}/>
-            {/if}
-
-              <input type="hidden" name="voice" bind:value={voice}>
-
-              <div class="w-full flex justify-between">
-                  <div class="flex gap-2">
-                    <Badge class="h-6" variant={voice === "trump" ? "default" : "outline"} onclick={selectVoice("trump")}>Trump</Badge>
-                    <Badge class="h-6" variant={voice === "peter" ? "default" : "outline"} onclick={selectVoice("peter")}>Peter</Badge>
-                    <Badge class="h-6" variant={voice === "obama" ? "default" : "outline"} onclick={selectVoice("obama")}>Obama</Badge>
-                  </div>
-                  <button class="w-20 {buttonVariants({ variant: "default" })}">Submit</button>
-              </div>
-          </form>
-      </Card.Content>
-    </Card.Root>
+    {#if generationStep === "idle"}
+        <PromptInput />
+    {/if}
 </div>
