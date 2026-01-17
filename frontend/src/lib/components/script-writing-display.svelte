80cbcd86d6dae5b80b04880b5924ba50f16959fe
bedminer1 <alexander.seah.min.rui@gmail.com>
2026-01-17 22:07:04 +0800

    rendering vincent's canvas on writing script

--- frontend/src/lib/components/script-writing-display.svelte
+++ frontend/src/lib/components/script-writing-display.svelte
@@ -1,10 +1,40 @@
 <script lang="ts">
-	import { Textarea } from "$lib/components/ui/textarea/index.js";
+	import { videoState } from "$lib/stores/generation-data.svelte";
+	import UnityPlayer from "$lib/components/UnityPlayer.svelte";
+
+    let unityPlayer: UnityPlayer | undefined = $state();
+	let displayedScript = $state("");
+
+	function startTypewriter(text: string, speed = 1) {
+        displayedScript = "";
+        let i = 0;
+    
+        const timer = setInterval(() => {
+            if (i < text.length) {
+                displayedScript += text.charAt(i);
+                i++;
+            } else {
+                clearInterval(timer);
+            }
+        }, speed);
+    }
+
+    $effect(() => {
+        if (videoState) {
+            startTypewriter(videoState.full_script);
+        }
+    });
+	
+	$effect(() => {
+		setTimeout(() => {
+			unityPlayer?.changeBackgroundColor("#09090b");
+			unityPlayer?.startTyping(videoState.full_script);
+		}, 5000)
+	})
 </script>
 
-<div class="mx-auto max-w-4xl space-y-6 p-6">
-	hi from script writing
-	<canvas>
-
-	</canvas>
+<div class="flex flex-col justify-center items-center w-full h-[90vh] gap-6">
+	<div class="h-full w-[90%] rounded-3xl">
+		<UnityPlayer bind:this={unityPlayer}/>
+	</div>
 </div>
