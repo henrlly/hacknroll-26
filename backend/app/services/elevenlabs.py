9074c6668c8827acf4ea3346d04b8f7d36e123b8
Henry <80515759+henrlly@users.noreply.github.com>
2026-01-18 01:18:36 +0800

    comment out trump voice local

--- backend/app/services/elevenlabs.py
+++ backend/app/services/elevenlabs.py
@@ -15,7 +15,7 @@ from app.services.narration_timestamps import (
     extract_word_timestamps,
     transcribe_with_scribe_v2,
 )
-from app.services.trump_voice import generate_trump_voice
+# from app.services.trump_voice import generate_trump_voice
 from app.utils.alignment import (
     character_alignment_to_word_alignment,
     mock_word_alignment,
@@ -52,16 +52,16 @@ async def generate_speech(
         )
         alignment = mock_word_alignment(text)
     else:
-        if use_local:
-            generate_trump_voice(text, file_name)
-        else:
-            voices: dict[VoiceType, str] = {
-                "trump": "7ee05bf86c884881945ca034aeddbebb",
-                "obama": "4ce7e917cedd4bc2bb2e6ff3a46acaa1",
-                "peter": "a5c5987257a14018a90111ee52a4e71a"
-            }
-            audio = await fishClient.tts.convert(text="Saving this audio to a file!", reference_id=voices[voice])
-            save(audio, file_name)
+        # if use_local:
+            # generate_trump_voice(text, file_name)
+        # else:
+        voices: dict[VoiceType, str] = {
+            "trump": "7ee05bf86c884881945ca034aeddbebb",
+            "obama": "4ce7e917cedd4bc2bb2e6ff3a46acaa1",
+            "peter": "a5c5987257a14018a90111ee52a4e71a"
+        }
+        audio = await fishClient.tts.convert(text="Saving this audio to a file!", reference_id=voices[voice])
+        save(audio, file_name)
         with open(file_name, "rb") as f:
             transcription = await client.speech_to_text.convert(
                 model_id="scribe_v1", file=f
